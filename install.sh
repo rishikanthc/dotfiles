b924a5aca8b5a7fa28ec2f5c467ff79a08cdfe99
Rishikanth <r3chandr@ucsd.edu>
2019-06-19 20:22:07 -0700

    script

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo pacman --noconfirm -S yay
+sudo pacman --noconfirm -S playerctl
+sudo pacman --noconfirm -S neovim
+sudo pacman --noconfirm -S python-pywal
+sudo pacman --noconfirm -S rofi
+sudo pacman --noconfirm -S powerline-fonts
+yay --noconfirm -S nerd-fonts-source-code-pro
+yay --noconfirm -S otf-san-francisco
+yay --noconfirm -S system-san-francisco-font-git
+yay --noconfirm -S termite
